9da4661ebb5851b0e574b1e75591f37ca373ea92
RutujaPRane <rutujaprane1701@gmaiL.com>
2022-07-08 21:53:53 +0530

    changes added

---
+++ src/PracticePrograms/Arrays/FlippingImages.java
@@ -0,0 +1,37 @@
+package PracticePrograms.Arrays;
+
+import java.util.Arrays;
+
+public class FlippingImages {
+        public static void main(String[] args)
+        {
+
+            // Driver code
+            int[][] mat = { { 1, 1, 0, 0 },
+                    { 1, 0, 0, 1 },
+                    { 0, 1, 1, 1 },
+                    { 1, 0, 1, 0 } };
+
+            int[][] matrix = flipped_Invert_Image(mat);
+            for (int[] matele : matrix) {
+                System.out.print(Arrays.toString(matele));
+            }
+        }
+
+        public static int[][] flipped_Invert_Image(int[][] mat)
+        {
+            for (int[] row : mat) {
+                for (int i = 0; i < (mat[0].length + 1) / 2; i++)
+                {
+                    // swap
+                    int temp = row[i] ^ 1;
+                    row[i] = row[mat[0].length - 1 - i] ^ 1;
+                    row[mat[0].length - 1 - i] = temp;
+                }
+            }
+
+            // return Flipped and Inverted image
+            return mat;
+        }
+    }
+
