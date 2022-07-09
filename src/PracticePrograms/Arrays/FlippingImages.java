360280d74ac6c89deb9d9a584b374033d7d96ad7
RutujaPRane <rutujaprane1701@gmaiL.com>
2022-07-09 20:33:16 +0530

    changes added

--- src/PracticePrograms/Arrays/FlippingImages.java
+++ src/PracticePrograms/Arrays/FlippingImages.java
@@ -3,35 +3,28 @@ package PracticePrograms.Arrays;
 import java.util.Arrays;
 
 public class FlippingImages {
-        public static void main(String[] args)
-        {
-
-            // Driver code
-            int[][] mat = { { 1, 1, 0, 0 },
-                    { 1, 0, 0, 1 },
-                    { 0, 1, 1, 1 },
-                    { 1, 0, 1, 0 } };
-
-            int[][] matrix = flipped_Invert_Image(mat);
+        public static void main(String[] args) {
+            int[][] image = { { 1, 1, 0,0},
+                    { 1, 0, 0 ,1},
+                    { 0, 1, 1,1},
+                    { 1, 0, 1,1} };
+            int[][] matrix = flipped_Invert_Image(image);
             for (int[] matele : matrix) {
                 System.out.print(Arrays.toString(matele));
             }
         }
-
-        public static int[][] flipped_Invert_Image(int[][] mat)
-        {
-            for (int[] row : mat) {
-                for (int i = 0; i < (mat[0].length + 1) / 2; i++)
-                {
-                    // swap
-                    int temp = row[i] ^ 1;
-                    row[i] = row[mat[0].length - 1 - i] ^ 1;
-                    row[mat[0].length - 1 - i] = temp;
+            public static int[][] flipped_Invert_Image(int[][] image)
+            {
+                int m = image.length;
+                int n = image[0].length;
+                for (int i = 0; i < m; i++) {
+                    for (int j = 0; j < (n + 1) / 2; j++) {
+                        // swap
+                        int temp = image[i][j] ^ 1;
+                        image[i][j] = image[i][n - 1 - i] ^ 1;
+                        image[i][n - 1 - i] = temp;
+                    }
                 }
+                return image;
             }
-
-            // return Flipped and Inverted image
-            return mat;
-        }
-    }
-
+}
