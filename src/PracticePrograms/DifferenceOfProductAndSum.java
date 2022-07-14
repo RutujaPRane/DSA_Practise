e77183709a0295d3c29b4a73c605832003abe3f6
RutujaPRane <rutujaprane1701@gmaiL.com>
2022-07-14 19:25:47 +0530

    changes added

--- src/PracticePrograms/DifferenceOfProductAndSum.java
+++ src/PracticePrograms/DifferenceOfProductAndSum.java
@@ -7,8 +7,9 @@ public class DifferenceOfProductAndSum {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the no.");
         int n = sc.nextInt();
-        int product = 1,sum = 0;
-        while (n >= 0) {
+        int product = 1;
+        int sum = 0;
+        while (n > 0) {
             product= product *(n % 10);
             sum = sum + (n % 10);
             n = n / 10;
