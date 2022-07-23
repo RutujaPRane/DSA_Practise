95e1bb5ee888610ed7417b6bd706adbd92db6b98
RutujaPRane <rutujaprane1701@gmaiL.com>
2022-07-23 22:15:05 +0530

    changes added

---
+++ src/Strings/Comparison.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class Comparison {
+    public static void main(String[] args) {
+        String a = "Rutuja";
+        String b = "Rutuja";
+
+//        System.out.println(a == b);
+
+        String Name1 = new String("Rutu");
+        String Name2 = new String("Rutu");
+
+        System.out.println(Name1 == Name2);
+        System.out.println(Name1.charAt(0));
+        System.out.println(Name1.equals(Name2));
+    }
+}
