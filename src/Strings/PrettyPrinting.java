6b007a65ba5e6a7fec91fdc098f72b0945ae964e
RutujaPRane <rutujaprane1701@gmaiL.com>
2022-07-23 22:37:23 +0530

    changes added

---
+++ src/Strings/PrettyPrinting.java
@@ -0,0 +1,11 @@
+package Strings;
+
+import static java.lang.Math.PI;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 23.34321f;
+//        System.out.printf("Formated number is %.2f ",a);
+        System.out.printf("Value of pi %.2f", Math.PI);
+    }
+}
