a5643fd9c040ff52b464471659697cc2e06ca491
RutujaPRane <rutujaprane1701@gmaiL.com>
2022-07-31 22:05:59 +0530

    changes added

---
+++ src/Recursion/PrintNumbers.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class PrintNumbers {
+    public static void main(String[] args) {
+        //print numbers till 1 to 5
+        print(1);
+    }
+    static void print(int n){
+        if(n==5){
+            System.out.println(5);
+            return;
+        }
+        System.out.println(n);
+        print(n + 1);
+    }
+
+}
