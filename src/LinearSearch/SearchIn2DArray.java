da3f779b9872aea47d581f56363fbdbb1c4820c7
RutujaPRane <rutujaprane1701@gmaiL.com>
2022-08-13 22:09:12 +0530

    changes added

---
+++ src/LinearSearch/SearchIn2DArray.java
@@ -0,0 +1,27 @@
+package LinearSearch;
+
+import java.util.Arrays;
+
+public class SearchIn2DArray {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {67,45,22,78},
+                {34,56,22,2},
+                {1,23,33}
+        };
+        int target = 56;
+        int[] ans = search(arr,target);
+        System.out.println(Arrays.toString(ans));
+    }
+    static int[] search(int[][] arr,int target){
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col]==target){
+                    return new int[]{row,col};
+                }
+            }
+
+        }
+        return new int[]{-1,-1};
+    }
+}
