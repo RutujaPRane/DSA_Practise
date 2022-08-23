11de1586be931c1268cfe7aff4d21163546e22d7
RutujaPRane <rutujaprane1701@gmaiL.com>
2022-08-23 21:22:45 +0530

    changes added

--- src/LinearSearch/MaxWealth.java
+++ src/LinearSearch/MaxWealth.java
@@ -8,20 +8,19 @@ public class MaxWealth {
                 {1,2,3},
                 {3,2,1}
                     };
-        System.out.println(ans);
-
+        System.out.println(maximumWealth(accounts));
     }
-    static int ans = Integer.MIN_VALUE;
     public static int maximumWealth(int[][] accounts) {
-        for (int person = 0; person < accounts.length; person++) {
+        int ans = Integer.MIN_VALUE;
+        for (int[] ints : accounts) {
             int sum = 0;
-            for (int account = 0; account < accounts[person].length; account++) {
-                sum +=accounts[person][account];
+            for (int anInt : ints) {
+                sum += anInt;
             }
-            if (sum>ans){
+            if (sum > ans) {
                 ans = sum;
             }
-
-        }return ans;
+        }
+        return ans;
     }
 }
