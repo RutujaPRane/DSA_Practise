2bb6f53feea8264ec89520dacf396095a45979e0
RutujaPRane <rutujaprane1701@gmaiL.com>
2022-08-26 17:45:13 +0530

    changes added

---
+++ src/BinarySearch/BinarySearch.java
@@ -0,0 +1,27 @@
+package BinarySearch;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 7, 12, 15, 35, 37, 46, 55};
+        int target = -15;
+        int ans = Search(arr, target);
+        System.out.println(ans);
+    }
+
+    static int Search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
