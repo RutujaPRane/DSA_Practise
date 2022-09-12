8812276c061b49c80119e67e9fa58ed7b8774991
RutujaPRane <rutujaprane1701@gmaiL.com>
2022-09-12 16:55:58 +0530

    changes added

--- src/BinarySearch/PeakOfArray.java
+++ src/BinarySearch/PeakOfArray.java
@@ -13,10 +13,19 @@ public class PeakOfArray {
         while (start>=end){
             int mid = start + (end - start)/2;
             if(arr[mid] > arr[mid+1]){
+                //this may be the ans but look at LHS
+               // u are in desc part of array
                 end = mid;
             }else {
-                start = mid +1;
+                // u are in asc part and mid < mid+1
+                start = mid + 1;
             }
-        }return start;
+        }
+        // in the end, start == end and pointing to the largest number because of the 2 checks above
+        // start and end are always trying to find max element in the above 2 checks
+        // hence, when they are pointing to just one element, that is the max one because that is what the checks say
+        // more elaboration: at every point of time for start and end, they have the best possible answer till that time
+        // and if we are saying that only one item is remaining, hence cuz of above line that is the best possible ans
+        return start; // or return end as both are =
     }
 }
