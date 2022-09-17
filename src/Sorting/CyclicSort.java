b02576999e61b8dc9b3346554076a4b718baf8a7
RutujaPRane <rutujaprane1701@gmaiL.com>
2022-09-17 19:48:33 +0530

    changes added

---
+++ src/Sorting/CyclicSort.java
@@ -0,0 +1,30 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {2,1,3,4,5};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort (int[] arr){
+        int i = 0;
+
+        while (i<arr.length){
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]){
+                swap(arr, i, correct);
+            }else{
+                i++;
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
